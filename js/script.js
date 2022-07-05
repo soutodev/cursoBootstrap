af6b858ffd0e48f8598a8ebbb4f9e9120c4b0bb5
Kevin Souto <kevinsoutods@gmail.com>
2022-07-04 22:40:50 -0300

    Finalizando o projeto com ScrollTo

--- js/script.js
+++ js/script.js
@@ -119,7 +119,7 @@ $(document).ready(function( ) {
             $(boxes).fadeIn();
         } else {
             $(boxes).each(function() {
-                if(!(this).hasClass(type)){
+                if(!$(this).hasClass(type)){
                     $(this).fadeOut('slow');
                 } else {
                     $(this).fadeIn();
@@ -127,5 +127,42 @@ $(document).ready(function( ) {
             });
         }
     }
+
+    //scrol para seções
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+
+        let btnId = $(this).attr('id');
+
+        if(btnId == 'about-menu') {
+            scrollTo = aboutSection;
+        } else if (btnId == 'services-menu') {
+            scrollTo = servicesSection;
+        } else if (btnId == 'team-menu') {
+            scrollTo = teamSection;
+        } else if (btnId == 'portfolio-menu') {
+            scrollTo = portfolioSection;
+        } else if (btnId == 'contact-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70 
+         }, 1500);
+    });
+
+    
 
 });
